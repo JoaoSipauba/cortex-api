414fb7ebad8f4851ca699a9b077916072e582bf1
JoaoSipauba <joaosipauba@hotmail.com>
2022-04-10 03:39:43 -0300

    fix: habilitando uso das rotas criadas no express

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
 import express from "express";
 
+import { routes } from "./routes";
+
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => console.log("server running on port 3333"));
