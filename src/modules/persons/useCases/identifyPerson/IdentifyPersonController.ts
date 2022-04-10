0d7732ea66209faec5fe0d4e72c7e3f580bb1eff
JoaoSipauba <joaosipauba@hotmail.com>
2022-04-10 09:02:05 -0300

    feat: criando metodos esqueleto dos metodos para a rota /identify

--- src/modules/persons/useCases/identifyPerson/IdentifyPersonController.ts
+++ src/modules/persons/useCases/identifyPerson/IdentifyPersonController.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from "express";
 
+import { IdentifyPersonUseCase } from "./IdentifyPersonUseCase";
+
 export class IdentifyPersonController {
     async handle(request: Request, response: Response) {
-        return response.status(200).json();
+        const { imgUrl, groupId } = request.body;
+
+        const identifyPersonUseCase = new IdentifyPersonUseCase();
+        const result = await identifyPersonUseCase.execute({
+            imgUrl,
+            groupId,
+        });
+
+        return response.status(200).json(result);
     }
 }
--- src/modules/persons/useCases/identifyPerson/IdentifyPersonUseCase.ts
+++ src/modules/persons/useCases/identifyPerson/IdentifyPersonUseCase.ts
@@ -1,14 +1,46 @@
+interface IIdentifyPerson {
+    imgUrl: string;
+    groupId: string;
+}
 export class IdentifyPersonUseCase {
-    async execute() {
+    async execute({ imgUrl, groupId }: IIdentifyPerson) {
         // should have valid image url
+        if (!imgUrl) {
+            throw new Error("imgUrl not found");
+        }
 
         // should have groupIdentifier
+        if (!groupId) {
+            throw new Error("groupId not found");
+        }
 
         // Azure should return faceId in /detect path
+        const faceId = await this.sendUrlToAzure(imgUrl);
 
         // Azure should return personId in /identify path
+        const personId = await this.matchesPersonWithFaceId(faceId);
 
         // Azure should return a list with personal datas
-        return null;
+        const personData = await this.getPersonDataWithPersonId(personId);
+
+        return personData;
+    }
+
+    private async sendUrlToAzure(imgUrl: string) {
+        const faceId = "";
+
+        return faceId;
+    }
+
+    private async matchesPersonWithFaceId(faceId: string) {
+        const personId = "";
+
+        return personId;
+    }
+
+    private async getPersonDataWithPersonId(personId: string) {
+        const data = "";
+
+        return data;
     }
 }
