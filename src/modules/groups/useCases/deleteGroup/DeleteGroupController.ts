42fea5f13744fdd3f8d1b3ea78def7c012a5fc3c
JoaoSipauba <joaosipauba@hotmail.com>
2022-04-10 16:38:53 -0300

    feat: adicionando funcionalidade de excluir grupo

--- src/modules/groups/useCases/deleteGroup/DeleteGroupController.ts
+++ src/modules/groups/useCases/deleteGroup/DeleteGroupController.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
 
+import { DeleteGroupUseCase } from "./DeleteGroupUseCase";
+
 export class DeleteGroupController {
     async handle(request: Request, response: Response) {
-        return response.status(200).json();
+        const { groupId } = request.query as {
+            groupId: string;
+        };
+
+        const deleteGroupUseCase = new DeleteGroupUseCase();
+        const result = deleteGroupUseCase.execute({ groupId });
+
+        return response.status(200).send(result);
     }
 }
--- src/modules/groups/useCases/deleteGroup/DeleteGroupUseCase.ts
+++ src/modules/groups/useCases/deleteGroup/DeleteGroupUseCase.ts
@@ -1,7 +1,24 @@
-export class DeleteGroupUseCase {
-    async execute() {
-        // Group should exists
+import api from "../../../../service/azureApi";
 
-        return null;
+interface IDeleteGroup {
+    groupId: string;
+}
+export class DeleteGroupUseCase {
+    async execute({ groupId }: IDeleteGroup) {
+        // groupId should exists
+        if (!groupId) {
+            throw new Error("groupId not found");
+        }
+
+        // should remove a group
+        try {
+            const result = await api.delete(`/largepersongroups/${groupId}`);
+
+            return result.data;
+        } catch (error: any) {
+            const errorMessage = error.response.data.error.message;
+
+            throw new Error(errorMessage || "Erro ao remover grupo");
+        }
     }
 }
